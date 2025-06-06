62ed2ef0ccc63d35f3d80bac69fe0876bfc8c9aa
GioZacca12 <giozak8@gmail.com>
2025-06-06 11:34:20 +0200

    betto fix

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -69,7 +69,7 @@ const Login = () => {
         <Container component="main" maxWidth="xs">
             <Box
                 sx={{
-                    marginTop: 8,
+                    paddingTop: 8,
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
