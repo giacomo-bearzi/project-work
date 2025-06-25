61374a0af91d076d9bfda862cf636604b2be8ef0
AndreaDelBianco <162106558+AndreaDelBianco@users.noreply.github.com>
2025-06-25 14:08:16 +0200

    Added UserActionsToolbar

---
+++ frontend/src/features/dashboard-users/components/UserActionsToolbar.tsx
@@ -0,0 +1,31 @@
+// components/UserActionsToolbar.tsx
+import { TextField, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+export const UserActionsToolbar = ({
+  searchTerm,
+  onSearchChange,
+  onAddUser,
+}: {
+  searchTerm: string;
+  onSearchChange: (value: string) => void;
+  onAddUser: () => void;
+}) => (
+  <div className="flex justify-between mb-2 mt-2 p-2">
+    <TextField
+      label="Cerca utente"
+      variant="outlined"
+      value={searchTerm}
+      onChange={(e) => onSearchChange(e.target.value)}
+      size="small"
+      sx={{ width: 300 }}
+    />
+    <Button
+      variant="contained"
+      onClick={onAddUser}
+      startIcon={<AddIcon />}
+    >
+      Aggiungi Utente
+    </Button>
+  </div>
+);
